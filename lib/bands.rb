af0c8d6929143683bf574889551833dfb94165b4
Chris Underwood <cunderwoodmn@gmail.com>
2015-09-02 14:56:01 -0700

    fix multiple issues that were keeping RSpec from running:

--- lib/bands.rb
+++ lib/bands.rb
@@ -1,6 +1,6 @@
-Class Band < ActiveRecord::Base
+class Band < ActiveRecord::Base
 
-  has_and_belongs_to_man(:venues)
+  has_and_belongs_to_many(:venues)
   validates :name, presence: true, uniqueness: true
   before_save(:capitalize_name)
 
--- lib/venues.rb
+++ lib/venues.rb
@@ -1,12 +1,12 @@
-Class Venues < ActiveRecord::Base
+class Venues < ActiveRecord::Base
 
-  has_and_belongs_to_man(:bands)
+  has_and_belongs_to_many(:bands)
   validates :name, presence: true, uniqueness: true
   before_save(:capitalize_name)
 
   private
 
-  define_mentod(:capitalize_name) do
+  define_method(:capitalize_name) do
     self.name = name().titlecase()
   end
 end
