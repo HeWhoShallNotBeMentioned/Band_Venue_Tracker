aa08c9721079369866001ff9f1e4f31cf3849ecc
Chris Underwood <cunderwoodmn@gmail.com>
2015-09-02 13:26:21 -0700

    fixed spelling on capitalize method for callbacks

--- lib/venues.rb
+++ lib/venues.rb
@@ -4,9 +4,9 @@ Class Venues < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true, length { minimum: 2 }
   before_save(:capitalize_name)
 
-  private 
+  private
 
   define_method(:capitalize_name) do
-    self.name = (name().capitilize())
+    self.name = (name().capitalize())
   end
 end
