e09809673ce742ae1787b8087cd8550e96443018
Chris Underwood <cunderwoodmn@gmail.com>
2015-09-02 16:29:04 -0700

    add the ability to edit and delete bands

--- app.rb
+++ app.rb
@@ -14,9 +14,32 @@ get '/bands' do
   erb(:bands)
 end
 
+get '/bands/:id' do
+  id = params.fetch('id').to_i
+  @band = Band.find(id)
+
+  erb(:band)
+end
+
 post '/bands/new' do
   name = params.fetch('band_name')
   @band = Band.create({name: name})
   @bands = Band.all
-  erb(:bands)  
+  erb(:bands)
+end
+
+patch '/bands/:id/update' do
+  id = params.fetch('id')
+  @band = Band.find(id)
+  name = params.fetch('band_name', @band.name)
+  @band.update({name: name})
+  redirect("/bands/#{@band.id}")
+end
+
+delete '/bands/:id/delete' do
+  id = params.fetch('id').to_i
+  band = Band.find(id)
+  band.destroy
+  @bands = Band.all
+  erb(:bands)
 end
