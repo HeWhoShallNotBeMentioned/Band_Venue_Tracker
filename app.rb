40972a9e3790347835106533292acfc6c721e9c1
Chris Underwood <cunderwoodmn@gmail.com>
2015-09-02 15:54:38 -0700

    add code to the app.rb and bands.erb to allow the user to add a band.

--- app.rb
+++ app.rb
@@ -13,3 +13,10 @@ get '/bands' do
   @bands = Band.all()
   erb(:bands)
 end
+
+post '/bands/new' do
+  name = params.fetch('band_name')
+  @band = Band.create({name: name})
+  @bands = Band.all
+  erb(:bands)  
+end
