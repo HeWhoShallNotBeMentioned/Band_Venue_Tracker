9a7ee7c71d4062b509294e5fbad42b09797a22b9
Chris Underwood <cunderwoodmn@gmail.com>
2015-09-02 13:14:28 -0700

    create Rakefile

---
+++ lib/bands.rb
@@ -0,0 +1,12 @@
+Class Band < ActiveRecord::Base
+
+  has_and_belongs_to_man(:venues)
+  validates :name, presence: true, uniqueness: true, length { minimum: 2 }
+  before_save(:capitalize_name)
+
+  private
+
+  define_method(:capitalize_name) do
+    self.name = (name().capitilize())
+  end
+end
---
+++ lib/venues.rb
@@ -0,0 +1,12 @@
+Class Venues < ActiveRecord::Base
+
+  has_and_belongs_to_man(:bands)
+  validates :name, presence: true, uniqueness: true, length { minimum: 2 }
+  before_save(:capitalize_name)
+
+  private
+
+  define_method(:capitalize_name) do
+    self.name = (name().capitilize())
+  end
+end
