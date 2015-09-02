c28f163817794de12aad86c9a7068e593688dfe2
Chris Underwood <cunderwoodmn@gmail.com>
2015-09-02 13:42:42 -0700

    add bands_venues join table via migration

---
+++ db/migrate/20150902203625_bands_venues_join_table.rb
@@ -0,0 +1,8 @@
+class BandsVenuesJoinTable < ActiveRecord::Migration
+  def change
+    create_table(:bands_venues) do |t|
+      t.column(:band_id, :integer)
+      t.column(:venue_id, :integer)
+    end
+  end
+end
