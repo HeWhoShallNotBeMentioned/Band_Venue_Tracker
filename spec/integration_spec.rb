1e38741feacb9e23e531c09c5b56114d56e738fa
Chris Underwood <cunderwoodmn@gmail.com>
2015-09-03 11:08:54 -0700

    added 2 passing unit tests to add a venue and a band

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -18,5 +18,27 @@ describe('visting the home page', {:type => :feature}) do
     expect(page).to have_content('All Venues')
   end
 
+  describe 'adding a new band', type: :feature do
+    it 'adds a band to database' do
+      visit '/'
+      click_link 'Bands'
+      fill_in('band_name', with: "the sunday's")
+      click_button('Add Your Band')
+      expect(page).to have_content("The Sunday's")
+    end
+
+  end
+
+  describe 'adding a new venue', type: :feature do
+    it 'adds a venue to database' do
+      visit '/'
+      click_link 'Venues'
+      fill_in('venue_name', with: "the met")
+      click_button('Add Your Venue')
+      expect(page).to have_content("The Met")
+    end
+
+  end
+
 
 end
