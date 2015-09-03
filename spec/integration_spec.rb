9982eceb504f0d2c18ecdbfb0704792918692f89
Chris Underwood <cunderwoodmn@gmail.com>
2015-09-03 11:02:47 -0700

    added 3 basic unit tests to make sure the main pages are available

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,1 +1,22 @@
 require 'spec_helper'
+
+describe('visting the home page', {:type => :feature}) do
+  it("makes sure the home page is available") do
+    visit("/")
+    expect(page).to have_content("Welcome to the Band and Venue tracker.")
+  end
+
+  it('make sure the Band page is available') do
+    visit('/')
+    click_link('Bands')
+    expect(page).to have_content('All Bands')
+  end
+
+  it('goes to venues page') do
+    visit('/')
+    click_link('Venues')
+    expect(page).to have_content('All Venues')
+  end
+
+
+end
